155e9420515f07731ebfb3ffbb161b806d684f85
Sam Gomez Olvera <samyolvera1@gmail.com>
2019-02-11 23:36:11 -0500

    added Node class that includes abstract methods

---
+++ src/Huffman/Node.java
@@ -0,0 +1,25 @@
+package Huffman;
+import java.util.Comparator;
+
+abstract class Node implements Comparable<Node> {
+   abstract int height();
+   abstract int count();
+   abstract boolean isLeaf();
+   abstract boolean isFull();
+   abstract boolean insertSymbol(int length, int symbol);
+   abstract Node left();
+   abstract Node right();
+   abstract int symbol();
+   
+
+    @Override
+    public int compareTo(Node o) {
+        if(count() != o.count()) {
+            return count() - o.count();
+        } else {
+            return height() - o.height();
+        }
+    }
+}
+
+
